e4188142606fbb9f54d779c1e8b8700011afcc42
Pavel <verbipo@gmail.com>
2024-03-05 10:24:02 +0100

    feat: fromJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -178,9 +178,13 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
-}
+// function fromJSON(proto, json) {
+//   const obj = Object.create(proto);
+//   Object.assign(obj, JSON.parse(json));
+//   return obj;
+// }
+const fromJSON = (proto, json) =>
+  Object.assign(Object.create(proto), JSON.parse(json));
 
 /**
  * Sorts the specified array by country name first and city name
